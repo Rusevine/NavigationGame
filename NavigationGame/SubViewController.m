f7b2a21be27c4a831b9e2f214c7a3e4e3e2aa44e
Wiljay <wiljay.flores@gmail.com>
2018-08-09 02:02:56 -0400

    finished my segues

---
+++ NavigationGame/SubViewController.m
@@ -0,0 +1,37 @@
+//
+//  SubViewController.m
+//  NavigationGame
+//
+//  Created by Wiljay Flores on 2018-08-09.
+//  Copyright © 2018 wiljay. All rights reserved.
+//
+
+#import "SubViewController.h"
+
+@interface SubViewController ()
+
+@end
+
+@implementation SubViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.navigationItem.rightBarButtonItem setTitle:@"Front"];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
